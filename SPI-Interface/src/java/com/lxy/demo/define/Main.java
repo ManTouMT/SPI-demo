d0896db08746d11cf7db5c0efd1474abb0ac7162
id <1773013849@qq.com>
2024-07-08 18:02:37 +0800

    interface的打包设置：添加入口类

---
+++ SPI-Interface/src/java/com/lxy/demo/define/Main.java
@@ -0,0 +1,8 @@
+package com.lxy.demo.define;
+
+public class Main {
+    public static void main(String[] args) {
+        LoggerService loggerService = new LoggerService();
+        loggerService.warnLog("123");
+    }
+}
