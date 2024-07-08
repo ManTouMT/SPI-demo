e2d359fa2794e0db0e86c656812271d49721a7c2
id <1773013849@qq.com>
2024-07-08 18:35:12 +0800

    重新打包interface

--- SPI-Service/src/java/com/lxy/demo/impl/ServiceMain.java
+++ SPI-Service/src/java/com/lxy/demo/impl/ServiceMain.java
@@ -1,10 +1,14 @@
 package com.lxy.demo.impl;
 
+import com.lxy.demo.define.LoggerService;
+
 public class ServiceMain {
     public static void main(String[] args) {
         // 入口
         LogbackService logbackService = new LogbackService();
         logbackService.warnLog("warn logback");
+        LoggerService service = LoggerService.getService();
+        service.warnLog("123");
     }
 }
     
