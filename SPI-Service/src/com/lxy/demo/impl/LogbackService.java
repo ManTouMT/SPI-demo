d57c2c8fce8b135b86453c21c1dfa3868d4dfcd0
id <1773013849@qq.com>
2024-07-08 17:32:17 +0800

    logback的实现

--- SPI-Service/src/com/lxy/demo/impl/LogbackService.java
+++ SPI-Service/src/com/lxy/demo/impl/LogbackService.java
@@ -1,4 +1,20 @@
 package com.lxy.demo.impl;
 
-public class LogbackService {
+import com.lxy.demo.define.JavaLogService;
+
+public class LogbackService implements JavaLogService {
+    @Override
+    public void infoLog(String message) {
+        System.out.println("logback service infoLog: " + message);
+    }
+
+    @Override
+    public void warnLog(String message) {
+        System.out.println("logback service warnLog: " + message);
+    }
+
+    @Override
+    public void errorLog(String message) {
+        System.out.println("logback service errorLog: " + message);
+    }
 }
